728a5097027625aeec81d0954157a906e6c7eb2c
xmz <xiaomengzhi@rainbowcn.com>
2019-07-07 09:26:16 +0800

    单任务版爬虫

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	engine.Run(engine.Request{
-		//Url:        "http://www.zhenai.com/zhenghun",
-		// ParserFunc: parser.ParseCityList,
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
 
-		Url:        "http://album.zhenai.com/u/70034915",
-		ParserFunc: parser.ParseProfile,
+		//Url:        "http://album.zhenai.com/u/70034915",
+		//ParserFunc: parser.ParseProfile,
 	})
 
 }
